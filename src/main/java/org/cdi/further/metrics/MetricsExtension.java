8bd3987ddf32e5f858940c0aac776e100c5c23a5
Antoine Sabot-Durand <antoine@sabot-durand.net>
2015-10-22 12:20:49 +0200

    Add Metric as Qualifier in Extension

--- src/main/java/org/cdi/further/metrics/MetricsExtension.java
+++ src/main/java/org/cdi/further/metrics/MetricsExtension.java
@@ -1,12 +1,20 @@
 package org.cdi.further.metrics;
 
 
+import com.codahale.metrics.annotation.Metric;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 
 /**
  * @author Antoine Sabot-Durand
  */
 public class MetricsExtension implements Extension {
+
+    void addMetricAsQualifier(@Observes BeforeBeanDiscovery bdd) {
+        bdd.addQualifier(Metric.class);
+    }
 
 
 }
